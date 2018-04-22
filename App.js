33110694fcbbcba2234903240997eef3be247e62
santosjn <jeansjcsp@gmail.com>
2018-04-22 09:24:27 -0300

    adding a starting 'list' of places.

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
   state = {
-    placeName: ''
+    placeName: '',
+    places: []
   }
 
   placeNameChangedHandler = (event) => {    
@@ -12,15 +13,40 @@ export default class App extends React.Component {
     });
   };
 
+  placeSubmitHandler = () => {
+    if(this.state.placeName.trim() === '') { return; } 
+    else {
+      this.setState( 
+        (prevState) => {
+          return {places: prevState.places.concat(prevState.placeName)}
+        }
+      )
+    }
+  }
+
   render() {
+    const placesOutput = this.state.places.map((place, index) => {
+      return <Text key={index}>{place}</Text>;
+    });
+
     return (
-      <View style={styles.container}>         
-        <TextInput
-          style={{ width: 200 }} 
-          placeholder="An example text"
-          value={this.state.placeName}           
-          onChangeText={this.placeNameChangedHandler}
-        />
+      <View style={styles.container}>
+        <View style={styles.InputContainer} >
+          <TextInput
+            // style={{ width: 200 }} 
+            placeholder="An example text"
+            value={this.state.placeName}           
+            onChangeText={this.placeNameChangedHandler}
+            style={styles.placeInput}
+          />
+          <Button 
+          title="Add" 
+          style={styles.placeButton}
+          onPress={this.placeSubmitHandler}/>          
+        </View> 
+        <View>
+          {placesOutput}
+        </View>
       </View>
     );
   }
@@ -35,4 +61,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
+  InputContainer: {
+    // flex: 1,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  placeInput: {
+    width: "70%"
+  },
+  placeButton: {
+    width: "30%"
+  }
 });
